93ff559eeeff16131dcf1a0e4071ce015c7f077b
Dawntraoz <albasilventefu@gmail.com>
2023-07-19 23:12:20 +0200

    feat: add base styles and define the first properties on Hero as example

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,28 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			white: '#FFFFFF',
+			black: '#000000',
+			primary: {
+				red: {
+					DEFAULT: '#E5645D',
+					medium: '#EE9C97',
+					light: '#F5C7C5',
+					lightest: '#FBE6E5',
+				},
+				yellow: {
+					DEFAULT: '#FCD02D',
+					medium: '#FEE179',
+					light: '#FEEEB3',
+					lightest: '#FFF7DD'
+				}
+			}
+		},
+		fontFamily: {
+			sora: ['Sora', 'sans-serif'],
+			'open-sans': ['Open Sans', 'sans-serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
